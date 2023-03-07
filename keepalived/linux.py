60880d72f83ed9ae04582c4cb1c23e41d138b8fa
nser77 <104022475+nser77@users.noreply.github.com>
2023-03-07 02:41:48 +0100

    Create linux.py

---
+++ keepalived/linux.py
@@ -0,0 +1,23 @@
+import subprocess
+from subprocess import Popen, PIPE
+
+class LinuxInterface():
+  @staticmethod
+  def _readSubprocess(command):
+      with Popen(command, shell=True, stdout=PIPE) as p:
+          p.wait()
+          return p.communicate()[0]
+
+  @staticmethod
+  def _runSubprocess(command):
+      with Popen(command, shell=True) as p:
+          p.wait()
+          return p
+
+  @staticmethod
+  def getIpv4NonLocalBind():
+    r = LinuxInterface._readSubprocess('cat /proc/sys/net/ipv4/ip_nonlocal_bind').split(b"\n")
+    return(int(r[0]))
+
+class SystemdInterface(LinuxInterface):
+  pass
